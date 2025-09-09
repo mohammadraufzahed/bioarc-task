6f16e7c0458a24021603044d30091edec6243702
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2025-09-09 13:47:05 +0330

    ✨ (index.html): update language attribute to support Persian (fa-IR) and set text direction to right-to-left (rtl) for better localization ✨ (App.tsx): replace placeholder content with MainLayout component to improve structure ✨ (Navbar/index.tsx): create a new Navbar component for better modularity and organization ✨ (MainLayout/index.tsx): create MainLayout component to encapsulate layout structure and include Navbar ✨ (vite.config.ts): add vite-plugin-svgr and vite-tsconfig-paths for enhanced SVG handling and path resolution 📝 (tsconfig.json): add TypeScript configuration file to improve type checking and module resolution ✨ (package.json): add vite-plugin-svgr and vite-tsconfig-paths dependencies for new features 🌐 (assets/logo.svg): add new logo SVG asset for branding 🌐 (assets/avatar.png): add new avatar image asset for user representation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
-import { css } from "./styled-system/css";
+import MainLayout from "./layouts/MainLayout";
+
 export default function App() {
-  return (
-    <span
-      className={css({
-        color: "green.500",
-        _hover: {
-          color: "blue.400",
-        },
-      })}
-    >
-      Hello World from React
-    </span>
-  );
+  return <MainLayout></MainLayout>;
 }
---
+++ src/layouts/MainLayout/index.tsx
@@ -0,0 +1,9 @@
+import Navbar from "@/components/Navbar";
+
+export default function MainLayout() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
